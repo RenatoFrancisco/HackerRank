dac10c03fc664e94dbe3a48032356ee7ba6110e6
Renato Francisco <renatofrancisco@ubuntu.local>
2021-01-19 13:45:53 -0300

    Solution Beautiful Days at the Movies

--- src/HackerRankProblems/Program.cs
+++ src/HackerRankProblems/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using HackerRankProblems.Solutions; 
 
 namespace HackerRankProblems
 {
@@ -6,7 +7,8 @@ namespace HackerRankProblems
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var result = BeatifulDaysAtTheMovies.Resolve(20, 23, 6);
+            Console.WriteLine($"Result: {result}");
         }
     }
 }
---
+++ src/HackerRankProblems/Solutions/BeatifulDaysAtTheMovies.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Linq;
+
+namespace HackerRankProblems.Solutions 
+{
+    public class BeatifulDaysAtTheMovies 
+    {
+        public static int Resolve(int i, int j, int k) 
+        {
+            var counter = 0;
+            for (var num = i; num <= j; num++)
+            {
+                var reverseNum = ReverseNumber(num);
+                var diff = Math.Abs(num - reverseNum);
+                counter += diff % k == 0 ? 1 : 0;
+            }
+
+            return counter;
+        }
+
+        private static int ReverseNumber(int number) 
+        {
+            var left = number;
+            var reverseNumber = 0;
+            while(Convert.ToBoolean(left))
+            {
+                var r = left % 10;   
+                reverseNumber = reverseNumber * 10 + r;
+                left = left / 10;
+            }
+
+            return reverseNumber;
+        }
+    }
+}
