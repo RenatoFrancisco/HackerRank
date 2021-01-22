5b9783f0e8bf1dc5c4f536852f842b712788dc6e
RenatoFrancisco <renatofrancisco.s@gmail.com>
2021-01-22 14:41:26 -0300

    Solution 2  lonely integer

--- src/HackerRankProblems/Solutions/LonelyInteger.cs
+++ src/HackerRankProblems/Solutions/LonelyInteger.cs
@@ -5,13 +5,6 @@ namespace HackerRankProblems.Solutions
 {
     public class LonelyInteger
     {
-        public static int Resolve(int[] a)
-        {
-            var unique = 0;
-            foreach (var n in a)
-                if (a.Count(x => x == n) == 1) unique = n;  
-
-            return unique;
-        }
+        public static int Resolve(int[] a) => a.First(n => a.Count(x => x == n) == 1);
     }
 }
